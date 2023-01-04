7a57843a84b34d7381105eccdfa65564ad62d764
nimotomato <>
2023-01-04 11:40:46 +0100

    refactored graph

--- isitgoingtohell/graph/graph.py
+++ isitgoingtohell/graph/graph.py
@@ -1,34 +1,57 @@
-from isitgoingtohell.label_analysis.label_analysis import Dated_methods as DM
-from isitgoingtohell.label_analysis.label_analysis import Undated_methods as UM
+from isitgoingtohell.label_analysis.label_analysis import Statistics
 from isitgoingtohell.utils import load_csv
 import plotly.express as px
 import pandas as pd
+from isitgoingtohell.data_management.db_management import Database as DB
+from isitgoingtohell.scrapers.pipelines import REGIONS
 
+TABLENAME = 'geography'
 
 class Graph():
     def __init__(self):
-        self.dm = DM()
-        self.um = UM()
+        self.stats = Statistics()
+        db = DB()
         self.country_codes = load_csv("only_codes.csv")
+        query = f'SELECT * FROM {TABLENAME}'
+        db.cur.execute(query)
+        self.data = [{'score': i[0], 'date': i[1], 'region': i[2]} for i in db.cur.fetchall()]
+        db.connection.close()
 
-    def draw_choropleth(self, figure_settings):
-        fig = figure_settings
-        fig["layout"].pop("updatemenus")
-        fig.show()
+    def add_country_codes(self, formated_ratios, region):
+        # Add country codes for a region.
+        ratios_with_country_codes = []
 
+        for country in self.country_codes:
+            if country['region'].lower() == region:
+                for score in formated_ratios:
+                    if score['region'] == region:
+                        item = {}
+                        item['country_code'] = country['code']
+                        item['score'] = score['score']
+                        item['date'] = score['date']
+                        item['region'] = score['region']
+                        ratios_with_country_codes.append(item)
 
-class Dated_graph(Graph):
-    def __init__(self, geography_data, column_names):
-        super().__init__()
-        # Get data
-        self.geography_data = self.dm.map_all_regions_dated(geography_data)
-        self.column_names = geography_data[0].keys()
-        self.dataframe = pd.DataFrame(geography_data, columns=column_names).sort_values(by = 'date')
+        return ratios_with_country_codes 
 
-    def set_choropleth_settings(self, df):
-        # Set data into settings
+    def add_codes_all_regions(self, data) -> list:
+        # Adds country codes for all regions.
+        ratios_with_country_codes = []
+        for region in REGIONS:
+            ratios_with_country_codes.extend(self.add_country_codes(data, region))
+
+        return ratios_with_country_codes
+
+    def set_dataframe(self):
+        data_mapped_to_country_code = self.add_codes_all_regions(data=self.data)
+        columns = data_mapped_to_country_code[0].keys()
+        
+        return pd.DataFrame(data_mapped_to_country_code, columns=columns).sort_values(by = 'date')
+
+    def set_choropleth_settings(self, dataframe):
+        # Set settings
         figure_settings = px.choropleth(
-            df, 
+            dataframe, 
             locationmode="ISO-3", 
             locations="country_code",
             color="score",
@@ -38,32 +61,12 @@ class Dated_graph(Graph):
             range_color=[0.2, 1.2],
             color_continuous_scale=px.colors.diverging.RdYlGn
             )
+            
         return figure_settings
 
-    def draw_dated_choropleth(self):
-        settings = self.set_choropleth_settings(self.dataframe)
-        self.draw_choropleth(settings)
-
-
-class Undated_graph(Graph):
-    def __init__(self, geography_data_undated):
-        super().__init__()
-        self.geography_data = self.um.map_all_regions_undated(geography_data_undated)
-
-    def set_choropleth_settings(self, mapped_geography_data):
-        figure_settings = px.choropleth(
-            mapped_geography_data, 
-            locationmode='ISO-3', 
-            locations='country_code',
-            color='score',
-            hover_name='region',
-            title ='IS IT GOING TO HELL?', 
-            range_color=[0.2, 1],
-            color_continuous_scale=px.colors.diverging.RdYlGn
-            )
-        return figure_settings
-        
-    def draw_undated_choropleth(self):
-        # Draws map from data in database
-        settings = self.set_choropleth_settings(self.geography_data)
-        self.draw_choropleth(settings)
+    def draw_choropleth(self):
+        df = self.set_dataframe()
+        figure_settings = self.set_choropleth_settings(df)
+        fig = figure_settings
+        fig["layout"].pop("updatemenus")
+        fig.show()
